2728942b8f3c5925262b2eae7b78d8c3d75487c9
Haswell <selimkayumov@gmail.com>
2022-08-21 04:50:15 +0200

    Added class for tree logic

--- Assets/Scripts/Tree.cs
+++ Assets/Scripts/Tree.cs
@@ -1,24 +1,35 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SceneManagement;
 using UnityEngine;
+using Weapons;
 
 public class Tree : MonoBehaviour
 {
     private bool _isChopped;
     private Rigidbody _rigidbody;
 
-    [SerializeField] private String _axeTag;
-
     private void Awake()
     {
         _isChopped = false;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (_isChopped) return;
+    //     
+    //     if (collision.gameObject.TryGetComponent<IWeapon>(out var _))
+    //     {
+    //         var weaponRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+    //         Break(weaponRigidbody);
+    //     }
+    // }
+
+    private void Break(Rigidbody weaponRigidbody)
     {
-        AddComponent<Rigidbody>();
+        _rigidbody = gameObject.AddComponent<Rigidbody>();
+        _rigidbody.drag = 0.25f;
+        _rigidbody.angularDrag = 0.5f;
+        _rigidbody.velocity = weaponRigidbody.velocity;
+
         _isChopped = true;
     }
 }
